92562c9a52afec043812e4d0106d49323b99a56c
ZX_Diablo <yakut2004@bk.ru>
2017-11-13 23:18:08 +0500

    Enable UMG and Slate

--- Source/UE4SampleChat/UE4SampleChat.Build.cs
+++ Source/UE4SampleChat/UE4SampleChat.Build.cs
@@ -8,12 +8,12 @@ public class UE4SampleChat : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
