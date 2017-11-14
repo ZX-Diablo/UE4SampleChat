994056f7b90356299099efc0f6da8b908207a06b
ZX_Diablo <yakut2004@bk.ru>
2017-11-15 00:15:04 +0500

    Menu properties and functions

--- Source/UE4SampleChat/Include/UE4SampleChatGameModeBase.h
+++ Source/UE4SampleChat/Include/UE4SampleChatGameModeBase.h
@@ -14,7 +14,34 @@ class UE4SAMPLECHAT_API AUE4SampleChatGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	/**
+	 * @brief Show main menu
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Interface")
+	void ShowMainMenu ();
+
+	/**
+	 * @brief Show chat menu
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Interface")
+	void ShowChatMenu ();
+
+protected:
+	virtual void BeginPlay () override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interface")
+	TSubclassOf<UUserWidget> MainMenu; ///< Main menu widget
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interface")
+	TSubclassOf<UUserWidget> ChatMenu; ///< Chat menu widget
+
+private:
+	void ShowMenuHelper (TSubclassOf<UUserWidget> Menu);
+	void CloseCurrentMenu ();
+
+private:
+	UPROPERTY()
+	UUserWidget* CurrentMenu;
 };
--- Source/UE4SampleChat/Src/UE4SampleChatGameModeBase.cpp
+++ Source/UE4SampleChat/Src/UE4SampleChatGameModeBase.cpp
@@ -2,6 +2,44 @@
 
 #include "Include/UE4SampleChatGameModeBase.h"
 
+#include "UserWidget.h"
 
+void AUE4SampleChatGameModeBase::ShowMainMenu ()
+{
+	this->ShowMenuHelper(this->MainMenu);
+}
 
+void AUE4SampleChatGameModeBase::ShowChatMenu ()
+{
+	this->ShowMenuHelper(this->ChatMenu);
+}
 
+void AUE4SampleChatGameModeBase::BeginPlay ()
+{
+	Super::BeginPlay();
+
+	this->ShowMainMenu();
+}
+
+void AUE4SampleChatGameModeBase::ShowMenuHelper (TSubclassOf<UUserWidget> Menu)
+{
+	if (Menu)
+	{
+		this->CloseCurrentMenu();
+
+		this->CurrentMenu = CreateWidget<UUserWidget>(this->GetWorld(), Menu);
+		if (this->CurrentMenu)
+		{
+			this->CurrentMenu->AddToViewport();
+		}
+	}
+}
+
+void AUE4SampleChatGameModeBase::CloseCurrentMenu ()
+{
+	if (this->CurrentMenu && this->CurrentMenu->IsInViewport())
+	{
+		this->CurrentMenu->RemoveFromViewport();
+		this->CurrentMenu = nullptr;
+	}
+}
