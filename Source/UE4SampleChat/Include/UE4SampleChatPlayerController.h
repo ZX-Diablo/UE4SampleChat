8b5ab635c4900d021153264ab9d58e86d9104267
ZX_Diablo <yakut2004@bk.ru>
2017-11-17 00:01:55 +0500

    Add empty player controller

---
+++ Source/UE4SampleChat/Include/UE4SampleChatPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UE4SampleChatPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4SAMPLECHAT_API AUE4SampleChatPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
